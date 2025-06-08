dcaffae35a87d4604e0a15d719955416d1846189
Bao Son <baoson20022001@gmail.com>
2025-06-08 21:37:08 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import './App.css';
 import Header from './components/header/Header';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Home from './pages/Home';
-// import Footer from './components/footer/Footer';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <>
+    <div className='overflow-hidden'>
       <Header />
       <Home />
-      {/* <Footer /> */}
-    </>
+      <Footer />
+
+    </div>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Hero from '../components/hero/Hero'
 
 const Home = () => {
   return (
-    <Hero />
+<></>
   )
 }
 
