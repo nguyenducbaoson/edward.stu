199da313fd74eab11caecacdf756ee6e0077a922
Bao Son <baoson20022001@gmail.com>
2025-06-12 23:04:18 +0700

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // <-- thêm dòng này
+import { BrowserRouter } from 'react-router-dom'; 
 import './index.css';
 import App from './App.tsx';
 
